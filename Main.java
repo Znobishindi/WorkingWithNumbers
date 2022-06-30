53d3cf9ec2d377f774d3492f5a46fe5875e9cbb5
Denis Znobishin <znobishindi@yandex.ru>
2022-06-30 12:53:15 +0300

    Files added

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Collections.sort(intList);
+        for (int x : intList) {
+            if (x > 0) {
+                if (x % 2 == 0) {
+                    System.out.println(x);
+                }
+            }
+        }
+    }
+}
+
+
+class StreamMain {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Stream<Integer> stream = intList.stream()
+                .filter(x -> x > 0)
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder());
+        stream.forEach(System.out::println);
+    }
+}
